ba8cfb7a19d7b86db7c1f6c9e2ee67931f2be8f4
Pratik Valvi <pratikvalvi@pop-os.localdomain>
2023-08-28 19:33:13 +0530

    Chatbot initialized

--- app/page.js
+++ app/page.js
@@ -1,113 +1,216 @@
-import Image from 'next/image'
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import clsx from "clsx";
+import { Bot, User } from "lucide-react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import Textarea from "react-textarea-autosize";
+import { LoadingCircle, SendIcon } from "@/icons/icons";
+
+const apiKey = process.env.NEXT_PUBLIC_API_KEY; // Replace with your API key
+const hostName = process.env.NEXT_PUBLIC_HOST;
+const model = process.env.NEXT_PUBLIC_MODEL; // Replace with model of your choice
+const apiUrl = `https://${hostName}/v1beta2/models/${model}:generateText`;
+
+const examples = [
+  "What is JavaScript?",
+  "Tell me some basic concepts of JavaScript.",
+  "What are data types in JavaScript?",
+];
 
 export default function Home() {
+  const [messages, setMessages] = useState([]);
+  const [userText, setUserText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const formRef = useRef(null);
+  const inputRef = useRef(null);
+  const inputText = useRef({
+    value: "",
+  });
+
+  const setInputText = (value) => {
+    inputText.current.value = value;
+  };
+
+  const disabled = loading;
+
+  useEffect(() => {
+    const generateAIMessage = async (textInput) => {
+      setLoading(true);
+      setError(null);
+      try {
+        const requestOptions = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt: { text: textInput }, // Use the stored user input as prompt text
+          }),
+        };
+        const response = await fetch(`${apiUrl}?key=${apiKey}`, requestOptions);
+        const jsonData = await response.json();
+
+        const newMessage = {
+          role: "assistant",
+          content: jsonData.candidates[0].output,
+        };
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+      } catch (error) {
+        setError(error);
+        const newMessage = {
+          role: "assistant",
+          content: "Something went wrong. Try Again!",
+        };
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+      } finally {
+        setLoading(false);
+        setUserText("");
+      }
+    };
+
+    if (userText && userText?.trim() !== "") {
+      const newMessage = {
+        role: "user",
+        content: userText,
+      };
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+      generateAIMessage(userText);
+    }
+  }, [userText]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputText.current.value && inputText.current.value?.trim() !== "") {
+      setUserText(inputText.current.value);
+      setInputText("");
+      inputRef.current.value = "";
+      inputRef.current?.focus();
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex flex-col items-center justify-between pb-40">
+      {messages.length > 0 ? (
+        messages.map((message, i) => (
+          <div
+            key={i}
+            className={clsx(
+              "flex w-full items-center justify-center border-b border-gray-200 py-8",
+              message.role === "user" ? "bg-white" : "bg-gray-100"
+            )}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <div className="flex w-full max-w-screen-md items-start space-x-4 px-5 sm:px-0">
+              <div
+                className={clsx(
+                  "p-1.5 text-white",
+                  message.role === "assistant" ? "bg-green-500" : "bg-black"
+                )}
+              >
+                {message.role === "user" ? (
+                  <User width={20} />
+                ) : (
+                  <Bot width={20} />
+                )}
+              </div>
+              <ReactMarkdown
+                className="prose mt-1 w-full break-words prose-p:leading-relaxed"
+                remarkPlugins={[remarkGfm]}
+                components={{
+                  // open links in new tab
+                  a: (props) => (
+                    <a {...props} target="_blank" rel="noopener noreferrer" />
+                  ),
+                }}
+              >
+                {message.content}
+              </ReactMarkdown>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div className="border-gray-200 sm:mx-0 mx-5 mt-20 max-w-screen-md rounded-md border sm:w-full">
+          <div className="flex flex-col space-y-4 p-7 sm:p-10">
+            <h1 className="text-lg font-semibold text-black">
+              Welcome to CodeGPT
+            </h1>
+            <p className="text-gray-500">
+              "Explore coding concepts and conquer challenges with your
+              dedicated coding buddy by your side."
+            </p>
+          </div>
+          <div className="flex flex-col space-y-4 border-t border-gray-200 bg-gray-50 p-7 sm:p-10">
+            {examples.map((example, i) => (
+              <button
+                key={i}
+                className="rounded-md border border-gray-200 bg-white px-5 py-3 text-left text-sm text-gray-500 transition-all duration-75 hover:border-black hover:text-gray-700 active:bg-gray-50"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setInputText(example);
+                  inputRef.current.value = example;
+                  inputRef.current?.focus();
+                }}
+              >
+                {example}
+              </button>
+            ))}
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      )}
+      <div className="fixed bottom-0 flex w-full flex-col items-center space-y-3 bg-gradient-to-b from-transparent via-gray-100 to-gray-100 p-5 pb-3 sm:px-0">
+        <form
+          ref={formRef}
+          onSubmit={(e) => handleSubmit(e)}
+          className="relative w-full max-w-screen-md rounded-xl border border-gray-200 bg-white px-4 pb-2 pt-3 shadow-lg sm:pb-3 sm:pt-4"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <Textarea
+            ref={inputRef}
+            tabIndex={0}
+            required
+            rows={1}
+            autoFocus
+            placeholder="Send a message"
+            // value={inputText.current.value}
+            onChange={(e) => setInputText(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                formRef.current?.requestSubmit();
+                e.preventDefault();
+              }
+            }}
+            spellCheck={false}
+            className="w-full pr-10 focus:outline-none"
+          />
+          <button
+            className={clsx(
+              "absolute inset-y-0 right-3 my-auto flex h-8 w-8 items-center justify-center rounded-md transition-all",
+              disabled
+                ? "cursor-not-allowed bg-white"
+                : "bg-green-500 hover:bg-green-600"
+            )}
+            disabled={disabled}
+          >
+            {loading ? (
+              <LoadingCircle />
+            ) : (
+              <SendIcon
+                className={clsx(
+                  "h-4 w-4",
+                  inputText.current.value?.length === 0
+                    ? "text-gray-300"
+                    : "text-white"
+                )}
+              />
+            )}
+          </button>
+        </form>
+        <p className="text-center text-xs text-gray-400">
+          Built with ❤ by Pratik Valvi using Google's Text Bison LLM as base
+          model.
+        </p>
       </div>
     </main>
-  )
+  );
 }
